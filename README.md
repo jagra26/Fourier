703bdef41ab489b09d0155cd9913aa359305bbb7
jagra26 <31116911+jagra26@users.noreply.github.com>
2021-08-10 10:41:07 -0300

    radix 2 n 3

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ pip install -r requirements.txt
 
 Para executar o programa em python utilize:
 ```
-python3 dft.py
+python3 -W ignore dft.py
 ```
 
 Para executar o programa em matlab, abra a pasta do repositorio no matlab. E execute o programa fft.m
 
-Os gráficos com a resposta do sinal estão salvos nos arquivos .png, dft_py.png sendo a saída do programa em python e fft_m.png a do matlab
+Os gráficos com a resposta do sinal estão salvos nos arquivos .png: dft_py.png, fft2_py.png, fft3_py.png sendos as saídas do programa em python e fft_m.png a do matlab
 
 Este repositório é referente a AB1 do aluno João Arthur Gaia. Da disciplina de Processamento Digital de Sinais, ministrada pelo professor Thiago Cordeiro.
--- dft.py
+++ dft.py
@@ -7,6 +7,12 @@ import matplotlib.pyplot as plt
 def factor (x, N, k, n):
   return x*cmath.exp(-(1j*2*math.pi*k*n)/N)
 
+def wb(N, k):
+  return cmath.exp((-2*math.pi*1j*k)/N)
+
+def wc(N, k):
+  return cmath.exp((-2*math.pi*1j*2*k)/N)
+
 def dft (signal, inverse=False):
   if inverse:
     way = -1
@@ -24,6 +30,66 @@ def dft (signal, inverse=False):
   else:
     return np.round(out)
 
+def check(base, number):
+  result = math.log(number, base)
+  integer = int(result)
+  if result - integer == 0:
+    return 0
+  else:
+    print("integer ",integer)
+    pow = 1+integer
+    return (2**pow) - number
+
+def fft2(signal):
+  N = len(signal)
+  if N == 1:
+    return signal
+  Aout = []
+  Bout= []
+  P = []
+  Q = []
+  for j in range(N):
+    if j%2 == 0: 
+      P.append(signal[j])
+    else:
+      Q.append(signal[j])
+  A = fft2(P)
+  B =  fft2(Q)
+  for j in range(min(len(A), len(B))):
+    Aout.append(A[j] + wb(N, j)*B[j])
+    Bout.append(A[j] - wb(N, j)*B[j])
+  out = Aout+Bout
+  return np.round(out)
+
+def fft3(signal):
+  N = len(signal)
+  if N == 1:
+    return signal
+  P = []
+  Q = []
+  R = []
+  A_out = []
+  B_out = []
+  C_out = []
+  for k in range(N):
+    if k % 3 == 0:
+      P.append(signal[k])
+    elif k % 3 == 1:
+      Q.append(signal[k])
+    else:
+      R.append(signal[k])
+  A_line = fft3(P)
+  B_Line = fft3(Q)
+  C_line = fft3(R)
+  
+  for k in range(min(len(B_Line), len(C_line))):
+    B_Line[k] *= wb(N, k)
+    C_line[k] *= wc(N, k)
+    A_out.append(A_line[k] + B_Line[k] + C_line[k])
+    B_out.append(A_line[k]+complex((-1/2),(-(3**(1/2))/2))*B_Line[k]+complex((-1/2),((3**(1/2))/2))*C_line[k])
+    C_out.append(A_line[k]+complex((-1/2),((3**(1/2))/2))*B_Line[k]+complex((-1/2),(-(3**(1/2))/2))*C_line[k])
+  out = A_out + B_out + C_out
+  return np.round(out)
 
 def plot(original, transformed, inverse, fname):
     plt.figure()
@@ -51,9 +117,23 @@ def plot(original, transformed, inverse, fname):
     plt.ylabel('Amplitude')
     plt.title('Inverse transform sequence')
     plt.savefig(fname)
-    plt.show()
 
 sig = np.array([6, 8, 5, 4, 5, 6])
 sig_dft = dft(sig)
 inv_dft = dft(sig_dft, inverse=True)
+pad = np.zeros(2)
+sig_dft2 = dft((np.concatenate((sig, pad), axis=0)))
+sig_fft2 = fft2(np.concatenate((sig, pad), axis=0))
+pad = np.zeros(3)
+sig_dft3 = dft((np.concatenate((sig, pad), axis=0)))
+sig_fft3 = fft3(np.concatenate((sig, pad), axis=0))
+inv_fft = dft(sig_fft2, inverse=True)
+print("DFT: ", sig_dft)
+print("DFT with padding 2: ", sig_dft2)
+print("radix 2: ", sig_fft2)
+print("DFT with padding 3: ", sig_dft3)
+print("radix 3: ", sig_fft3)
+print("IDFT: ", inv_fft)
 plot(sig, sig_dft, inv_dft, "dft_py.png")
+plot(sig, sig_fft2, inv_dft, "fft2_py.png")
+plot(sig, sig_fft3, inv_dft, "fft3_py.png")
